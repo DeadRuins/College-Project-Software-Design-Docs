83d1e1376fcb73568f6faf9a6b41fe9d9ed9043b
MISA2215 <misacchi.tomato@gmail.com>
2026-06-19 14:52:34 +0900

    Add files via upload
    sekisanondo.py：積算温度の計算
    import_temperature_*：温度データをCSVファイルにインポートするプログラム(温度データ入れる間隔を分単位に設定できるように変更)

--- import_temperature_simulation.py
+++ import_temperature_simulation.py
@@ -8,12 +8,15 @@ FILENAME = "temperature_log.csv"
 
 try:
     # input() で受け取った文字列を float（小数点対応の数値）に変換
-    user_input = input("計測間隔（秒）を入力してください（例: 5 や 0.5）: ")
-    INTERVAL_SEC = float(user_input)
+    user_input = input("計測間隔（分）を入力してください（例: 5 や 0.5）: ")
+    INTERVAL_SEC_INPUT = float(user_input)
     
-    if INTERVAL_SEC <= 0:
+    if INTERVAL_SEC_INPUT <= 0:
         print("エラー: 0より大きい数値を入力してください。")
         exit()
+
+    INTERVAL_SEC = INTERVAL_SEC_INPUT * 60.0
+
 except ValueError:
     print("エラー: 正しい数値を入力してください。")
     exit()
@@ -41,8 +44,8 @@ try:
             
         print(f"[{now}] 記録完了: {temperature} °C")
         
-        # 2秒ごとにデータを取得
-        time.sleep(2)
+        # 入力した秒ごとにデータを取得
+        time.sleep(INTERVAL_SEC)
 
 except KeyboardInterrupt:
     print("\n記録を停止しました。")
--- sekisanondo.py
+++ sekisanondo.py
@@ -1,17 +1,25 @@
-#積算温度の計算
-
 import csv
 
-with open("sekisanondo.csv", newline="") as f:
-    reader = csv.reader(f)
+file_path = "temperature_log.csv"
+temperatures = []
+
+# CSVファイルを開く
+with open(file_path, mode="r", encoding="utf-8") as file:
+    reader = csv.DictReader(file)  # ヘッダー名でデータにアクセスできる形式で読み込み
+
     for row in reader:
-        print(row)
+        # 文字列から浮動小数点数(double/float)に変換してリストに追加
+        temp_value = float(row["Temperature (°C)"])
+        temperatures.append(temp_value)
 
-tempera=[row]
+# 結果の確認
+print("読み込んだ温度リスト（double型）:")
+print(temperatures)
+#print(f"データの型: {type(temperatures[0])}")
 
-tempera_sum=0
+sekisanondo=0.0
+for temperature in temperatures:
+    sekisanondo = sekisanondo + temperature
 
-for temper in row:
-    tempera_sum=tempera_sum+temper
-
-print(tempera_sum)
+print("積算温度:")
+print(round(sekisanondo,1))
