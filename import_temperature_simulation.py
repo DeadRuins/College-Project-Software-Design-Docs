f549f9844981bfb6bb13c26f4d9b179308985c70
DeadRuins <wipoppi666@gmail.com>
2026-06-26 14:41:37 +0900

    Now fetches the data from DHT Sensor connected

--- import_temperature_simulation.py
+++ import_temperature_simulation.py
@@ -2,7 +2,12 @@ import csv
 import time
 from datetime import datetime
 import random
+import serial
+import board
+import adafruit_dht
 
+# 1. adafruit config
+dhtDevice = adafruit_dht.DHT22(board.D18)
 # 保存するCSVファイル名
 FILENAME = "temperature_log.csv"
 
@@ -31,20 +36,26 @@ print(f"{FILENAME} を作成しました。データ記録を開始します... 
 
 try:
     while True:
-        # 現在の時刻を取得
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
-        # センサデータの代わりに20.0〜25.0度のランダムな数値を生成
-        temperature = round(random.uniform(20.0, 25.0), 1)
-        
-        # CSVファイルに追記（mode='a'）
-        with open(FILENAME, mode="a", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow([now, temperature])
+        try:
+            # 現在の時刻を取得
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
-        print(f"[{now}] 記録完了: {temperature} °C")
-        
-        # 入力した秒ごとにデータを取得
+            # センサデータの代わりに20.0〜25.0度のランダムな数値を生成
+            temperature = dhtDevice.temperature
+            
+            # CSVファイルに追記（mode='a'）
+            with open(FILENAME, mode="a", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow([now, temperature])
+                
+            print(f"[{now}] 記録完了: {temperature} °C")
+            
+            # 入力した秒ごとにデータを取得
+        except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+            print(error.args[0])
+            continue
+            
         time.sleep(INTERVAL_SEC)
 
 except KeyboardInterrupt:
